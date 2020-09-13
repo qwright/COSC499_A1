4be07a6791f5aeb41c21b6d3454927c6ab64ec8e
quinn <qfwright@outlook.com>
2020-09-12 22:25:26 -0700

    cli args

--- src/example.c
+++ src/example.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include "example.h"
 
-int main()
-{
-	printf("This is some toy code for Capstone\n");
-	int a = 3;
-	int b = 2;
-	int s = sum(a,b);
-	printf("Sum of %d and %d : %d\n",a,b, s);
 
+int main(int argc, char *argv[])
+{
+	int a = -1;
+	int b = -1;
+	int ans = -1;
+	int opt;
+	while((opt = getopt(argc, argv, "m:")) != -1){
+	switch(opt){
+		case 'm':
+			if(strcmp(optarg, "sum") == 0){
+				prompt(&a, &b);
+				ans = sum(a,b);
+				printf("Result: %d\n", ans);
+			}
+			else if(strcmp(optarg, "sub") == 0){
+				prompt(&a, &b);
+				ans = subtract(a,b);
+				printf("Result: %d\n", ans);
+			}
+			else{
+				usage();
+			}
+			break;
+		default:
+			usage();
+			return 1;
+		}
+	}
 	return 0;
 }
 
@@ -22,3 +45,19 @@ int subtract(int num1, int num2)
 	return num1 - num2;
 }
 
+void usage(void)
+{
+	printf("Usage:\n");
+	printf("-<mode>\n");
+	printf("Modes available: sum, sub\n");
+}
+
+void prompt(int *n1, int *n2)
+{
+	printf("This is some toy code for Capstone\n");
+	printf("Enter first number:\n");
+	scanf("%d", n1);
+	printf("Enter second number:\n");
+	scanf("%d", n2);
+
+}
